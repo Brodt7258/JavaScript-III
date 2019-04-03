852e76d45e6987f11116230122e7d8758d18ae43
Tim Dill <brodt7258@gmail.com>
2019-04-02 21:53:32 -0400

    built CharacterStats constructor

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -33,6 +33,17 @@ GameObject.prototype.destroy = function() {
   * should inherit destroy() from GameObject's prototype
 */
 
+function CharacterStats(attrs) {
+  GameObject.call(this, attrs);
+  this.healthPoints = attrs.healthPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage`;
+}
+
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
